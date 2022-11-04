ea660cf8714e80a1034e4af4c804f45697fdd59e
Mo Yasser <alalmy475@gmail.com>
2022-11-04 16:02:35 +0200

    edit circleci config

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello react from circleci deployed in netlify</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hola amigos
+        </a>
       </header>
     </div>
   );
