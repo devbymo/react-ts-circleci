318a8a007643b5a022df1b23958aa06633cd05c0
Mo Yasser <alalmy475@gmail.com>
2022-11-04 15:02:08 +0200

    edit app file

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello react from circleci</p>
+        <p>Hello react from circleci deployed in netlify</p>
       </header>
     </div>
   );
